69670442eff99787ab59af1bd4c88ab9855e90c7
shinebyul <qufdl8382@gmail.com>
2025-01-05 19:46:03 +0900

    이벤트 객체 변경

--- src/main/java/com/example/cdc_payload/global/infra/kafka/in/ChangeLogConsumer.java
+++ src/main/java/com/example/cdc_payload/global/infra/kafka/in/ChangeLogConsumer.java
@@ -4,6 +4,7 @@ import com.example.cdc_payload.domain.interaction.model.Interaction;
 import com.example.cdc_payload.domain.interaction.repository.InteractionRepository;
 import com.example.cdc_payload.global.infra.kafka.out.NewPayloadData;
 import com.example.cdc_payload.global.infra.kafka.out.PayloadLogProducer;
+import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -16,7 +17,7 @@ public class ChangeLogConsumer {
     private final PayloadLogProducer payloadLogProducer;
 
     @KafkaListener(topics = "change_log", groupId = "change_log_group")
-    public void newCaptureEvent(Map<String, Object> event){
+    public void newCaptureEvent(List<Map<String, Object>> event){
         System.out.println("이벤트 수신...");
 
         //{ROW_ID=AAATNPAAHAAAALkAEy,
@@ -25,14 +26,20 @@ public class ChangeLogConsumer {
 //        TABLE_NAME=INTERACTION,
 //        SQL_REDO=insert into "C##DEEP"."INTERACTION"("IDX","COMMENT_IDX","EMOJI_IDX","USER_IDX") values ('19835','5','484','3');}
 
+        for (Map<String, Object> stringObjectMap : event) {
+            try {
+                Interaction interaction = interactionRepository.findByRowId( stringObjectMap.get("ROW_ID").toString());
 
-        //rowID로 조회해오기
-        Interaction interaction = interactionRepository.findByRowId(event.get("ROW_ID").toString());
+                payloadLogProducer.sendNewPayloadLogCaptureMessage(NewPayloadData.builder()
+                        .operation(stringObjectMap.get("OPERATION").toString())
+                        .tableName(stringObjectMap.get("TABLE_NAME").toString())
+                        .interaction(interaction).build());
 
-        //발행하기
-        payloadLogProducer.sendNewPayloadLogCaptureMessage(NewPayloadData.builder()
-                .operation(event.get("OPERATION").toString())
-                .tableName(event.get("TABLE_NAME").toString())
-                .interaction(interaction).build());
+            } catch (Exception e) {
+                // 예외 처리 및 로그 출력
+                System.err.println("이벤트 처리 중 오류 발생: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
     }
 }
