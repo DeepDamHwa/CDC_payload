aceb122911ba61fe79757df2f1201773af37b22d
shinebyul <qufdl8382@gmail.com>
2025-01-12 19:41:30 +0900

    role entity, repository 추가

---
+++ src/main/java/com/example/cdc_payload/domain/role/model/Role.java
@@ -0,0 +1,35 @@
+package com.example.cdc_payload.domain.role.model;
+
+import com.example.cdc_payload.domain.EventEntity;
+import com.example.cdc_payload.domain.user.model.Users;
+import com.example.cdc_payload.global.infra.kafka.out.NewPayloadData;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Role implements EventEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    private String name;
+
+    @OneToMany(mappedBy = "role")
+    private List<Users> users = new ArrayList<>();
+
+    @Override
+    public NewPayloadData toDto(String operation) {
+        return NewRolePayloadData.builder()
+                .operation(operation)
+                .roleIdx(idx)
+                .name(name)
+                .build();
+    }
+}
