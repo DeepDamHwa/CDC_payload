d422ea07d749b81c91c6c008ffa90d55667607f4
shinebyul <qufdl8382@gmail.com>
2025-01-12 19:44:29 +0900

    producer 수정

--- src/main/java/com/example/cdc_payload/global/infra/kafka/out/PayloadLogProducer.java
+++ src/main/java/com/example/cdc_payload/global/infra/kafka/out/PayloadLogProducer.java
@@ -1,5 +1,11 @@
 package com.example.cdc_payload.global.infra.kafka.out;
 
+import com.example.cdc_payload.domain.comment.model.NewCommentsPayloadData;
+import com.example.cdc_payload.domain.emoji.model.NewEmojiPayloadData;
+import com.example.cdc_payload.domain.interaction.model.NewInteractionPayloadData;
+import com.example.cdc_payload.domain.post.model.NewPostPayloadData;
+import com.example.cdc_payload.domain.role.model.NewRolePayloadData;
+import com.example.cdc_payload.domain.user.model.NewUsersPayloadData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
@@ -8,7 +14,8 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class PayloadLogProducer {
     private final KafkaTemplate<String,Object> kafkaTemplate;
-    public void sendNewPayloadLogCaptureMessage(NewPayloadData newPayloadData){
-        kafkaTemplate.send("payload_log", newPayloadData);
+    public void sendNewPayloadLogCaptureMessage(NewPayloadData newPayloadData, String topic){
+        kafkaTemplate.send(topic, newPayloadData);
+        //"payload_log"
     }
 }
